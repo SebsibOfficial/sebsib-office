7256f05fa8f70f215ff3b3b8a1b199b8fd2bcf3b
Yoseph <yosephten@gmail.com>
2022-02-02 21:43:29 +0300

    Refactored Sb_Text

--- src/components/Sb_Text/Sb_Text.tsx
+++ src/components/Sb_Text/Sb_Text.tsx
@@ -1,8 +1,22 @@
-import React from "react";
 import './Sb_Text.css';
 
-export default function Sb_Text(props:any){
-    return <p className = {'hv ' + props.font + " " + props.color + " " + props.align + " " + props.clamp}> 
+interface Props {
+   font?: 12 | 16 | 20 | 24 | 32 | 48 | 72,
+   weight?: 300 | 400 | 600 | 900,
+   color?: "--lightGrey" | "--secondary",
+   align?: "center" | "right" | "left",
+   clamp?: number,
+   children:  any
+}
+
+export default function Sb_Text(props:Props){
+    return <p style={
+        {'fontSize':props.font, 
+        'color':`var(${props.color})`, 
+        'fontWeight':props.weight,
+        'textAlign':props.align,
+        }} 
+        className = { props.clamp ? `hv clamp c-${props.clamp}` : 'hv'}> 
         {props.children} 
     </p>
 }
