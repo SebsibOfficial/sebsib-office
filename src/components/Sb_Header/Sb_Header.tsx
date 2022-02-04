a82a1aa6df2c4e8b82f95007bd41b62c5def1eaf
Yoseph <yosephten@gmail.com>
2022-02-04 23:24:33 +0300

    Refactored Sb_Header

--- src/components/Sb_Header/Sb_Header.tsx
+++ src/components/Sb_Header/Sb_Header.tsx
@@ -1,20 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Row } from "react-bootstrap";
 import Sb_Text from "../Sb_Text/Sb_Text";
 import './Sb_Header.css';
+import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
-    header?:string, // The Header text
-    bc_only?:boolean, // Only show BreadCrumb
-    visible?:boolean, // BreadCrumb visibility
-    limit?:number // The number of BreadCrumb links to be seen
+    header: string,
+    onBackClick(): any,
 }
 
 const Sb_Header = (props:Props) => {
     return(
-        <Row className="sb-header g-0" style={{display: props.bc_only ? 'none' : 'inherit'}}>
-            <Col>
-                <Sb_Text font="c-bold48" align="left">{props.header}</Sb_Text>
-            </Col>
+        <Row className="sb-header g-0">
+            <Row className="g-0">
+                <Col>
+                    <FontAwesomeIcon 
+                        icon={faLongArrowAltLeft} 
+                        className="sb-header-back-btn"
+                        onClick={() => props.onBackClick()}
+                    />
+                </Col>
+            </Row>
+            <Row className="g-0">
+                <Col>
+                    <Sb_Text font={48} weight={600}>{props.header}</Sb_Text>
+                </Col>
+            </Row>
         </Row>                            
     )
 }
