13306fc0c1184550d2a45ecad2cb82a8bf6d27d3
Yoseph <yosephten@gmail.com>
2022-02-11 16:11:05 +0300

    Some changes

--- src/screens/app/App.tsx
+++ src/screens/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import logo from '../../assets/logo.svg';
+import { Button, Col, Row } from 'react-bootstrap';
+import logo from '../../assets/officeLogoBlack.svg';
 import Sb_List_Item from '../../components/Sb_List_Item/Sb_List_Item';
 import './App.css';
 
@@ -8,9 +8,17 @@ type actionType = "REMOVE" | "SELECT-CHANGE";
 
 function App() {
   return (
-    <div style={{width:'15em', margin:'5em'}}>
-        <Sb_List_Item id='23' type='MEMBER' actionType='SELECT-CHANGE' text='First Name' 
-        onAction={(id:string, actionType:actionType) => console.log("id: "+id+" action: "+actionType)}/>
+    <div className='login-parent-screen'>
+      <div className='d-flex login-screen'>
+        <Row>
+          <Col>
+            <img src={logo} alt="Logo" />
+          </Col>
+        </Row>
+        <Row>
+          
+        </Row>
+      </div>
     </div>
   );
 }
