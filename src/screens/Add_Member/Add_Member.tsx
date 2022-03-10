80ba321be08138683715475eaaff6c6b9b4b891b
Yoseph <yosephten@gmail.com>
2022-03-10 22:51:41 +0300

    Gathering Data

--- src/screens/Add_Member/Add_Member.tsx
+++ src/screens/Add_Member/Add_Member.tsx
@@ -1,11 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import Sb_List from "../../components/Sb_List/Sb_List";
+import { actionType } from "../../components/Sb_List_Item/Sb_List_Item";
 import Sb_Text from "../../components/Sb_Text/Sb_Text";
 
 interface Props {
   pageType: "ADD" | "EDIT"
+}
+
+class AddMemberPayload {
+  constructor (mn: string, me: string, mu: string, mp: string, pi: string[]){
+    this.memberName = mn;
+    this.memberEmail = me;
+    this.memberUsername = mu;
+    this.memberPassword = mp;
+    this.projectInvolved = pi;
+  }
+  memberName: string;
+  memberEmail: string;
+  memberUsername: string;
+  memberPassword: string;
+  projectInvolved: string[];
 }
 
 export default function Add_Modify_Member(props:Props) {
@@ -19,35 +35,69 @@ export default function Add_Modify_Member(props:Props) {
        return navigate("/404");
     }
   },[location.state]);
+
+  /*############# STATES ############### */
+  const [memberName, setMemberName] = useState("");
+  const [memberEmail, setMemberEmail] = useState("");
+  const [memberUsername, setMemberUsername] = useState("");
+  const [memberPassword, setMemberPassword] = useState("");
+  const [projectsInvolved, setProjectsInvolved] = useState<string[]>([]);
   
+  /*------------- METHODS -------------- */
+  function projectSelectHandler (id: string, ac: actionType | undefined) {
+    if (ac === 'SELECTED'){
+      var pi = [...projectsInvolved];
+      pi.push(id);
+      setProjectsInvolved(pi);
+    }
+    else if (ac === 'UNSELECTED'){
+      var pi = [...projectsInvolved];
+      pi = pi.filter(projID => projID != id);
+      setProjectsInvolved(pi);
+    }
+  }
+
+  function saveAddButtonHandler () {
+    if (props.pageType === 'ADD'){
+      // Gather data
+      var payload = new AddMemberPayload(memberName, memberEmail, memberUsername, memberPassword, projectsInvolved);
+      // Send to API -> If successful
+        navigate('/dashboard/members', {state: true})
+    }
+    else {
+
+    }
+  }
+
   return (
     <Col>
       <Row className="mb-4">
         <Col md="3" className="me-4">
 					<Form.Group className="mb-3" controlId="AddMemberName">
 						<Form.Label><Sb_Text font={16}>Member Name</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Name"/>
+						<Form.Control size="sm" type="text" placeholder="Name" value={memberName} onChange={(e) => setMemberName(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberEmail">
 						<Form.Label><Sb_Text font={16}>Email</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Email"/>
+						<Form.Control size="sm" type="text" placeholder="Email" value={memberEmail} onChange={(e) => setMemberEmail(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberUsername">
 						<Form.Label><Sb_Text font={16}>Member Username</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Name"/>
+						<Form.Control size="sm" type="text" placeholder="Name" value={memberUsername} onChange={(e) => setMemberUsername(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberPassword">
 						<Form.Label><Sb_Text font={16}>Password</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Password"/>
+						<Form.Control size="sm" type="text" placeholder="Password" value={memberPassword} onChange={(e) => setMemberPassword(e.target.value)}/>
 					</Form.Group>
         </Col>
         <Col md="4">
           <Sb_Text font={16} weight={500}><p>Projects Involved In</p></Sb_Text>
           <Sb_List 
 					items={[{id:'1', text:'Agriculture In Gondar', }, {id:'2', text:'AIDS Prevalance', }, {id:'3', text:'Minamin Chala', }, {id:'4', text:'Minamin Chala', }]} 
-					listType="PROJECT" compType='SELECT' onAction={(id, ac) => console.log(id+" CLICKED "+ac)}/>
-          <Button className="mt-3" size="sm" style={{'float':'right'}}><Sb_Text font={12} color="--lightGrey">
-            {props.pageType === 'ADD' ? 'Add Member' : 'Save Changes'}
+					listType="PROJECT" compType='SELECT' onAction={(id, text, ac) => projectSelectHandler(id, ac)}/>
+          <Button className="mt-3" size="sm" style={{'float':'right'}} onClick={() => saveAddButtonHandler()}>
+            <Sb_Text font={12} color="--lightGrey">
+              {props.pageType === 'ADD' ? 'Add Member' : 'Save Changes'}
             </Sb_Text>
           </Button>
         </Col>
--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,8 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
 import { useLocation, useNavigate } from "react-router-dom";
 import Sb_Container from "../../components/Sb_Container/Sb_Container";
 import Sb_Text from "../../components/Sb_Text/Sb_Text";
+
+class SaveSettingsPayload {
+  constructor (on: string, em: string, op: string, np: string, l: string) {
+    this.orgName = on;
+    this.email = em;
+    this.oldPass = op;
+    this.newPass = np;
+    this.lang = l;
+  }
+  orgName: string;
+  email: string;
+  oldPass: string;
+  newPass: string;
+  lang: string;
+}
 
 export default function Settings () {
   let location = useLocation();
@@ -15,6 +30,19 @@ export default function Settings () {
     }
   },[location.state]);
   
+  /*############# STATES ############### */
+  const [orgName, setOrgName] = useState("");
+  const [email, setEmail] = useState("");
+  const [oldPass, setOldPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+  const [lang, setLang] = useState("ENGLISH");
+
+  /*------------- METHODS -------------- */
+  function saveChanges () {
+    var payload = new SaveSettingsPayload(orgName, email, oldPass, newPass, lang);
+    // Send to API
+  }
+
   return (
     <Col md="5">
       <Sb_Text font={16} weight={500}>Credentials</Sb_Text>
@@ -22,19 +50,19 @@ export default function Settings () {
         <div style={{'width':'70%'}}>
           <Form.Group className="mb-3" controlId="AddMemberUsername">
 						<Form.Label><Sb_Text font={16}>Organization Name</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Your Organization Name"/>
+						<Form.Control size="sm" type="text" placeholder="Your Organization Name" value={orgName} onChange={(e) => setOrgName(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberUsername">
 						<Form.Label><Sb_Text font={16}>Email</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Your Email"/>
+						<Form.Control size="sm" type="text" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberPassword">
 						<Form.Label><Sb_Text font={16}>Old Password</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="password" placeholder="Password"/>
+						<Form.Control size="sm" type="password" placeholder="Password" value={oldPass} onChange={(e) => setOldPass(e.target.value)}/>
 					</Form.Group>
           <Form.Group className="mb-3" controlId="AddMemberPassword">
 						<Form.Label><Sb_Text font={16}>New Password</Sb_Text></Form.Label>
-						<Form.Control size="sm" type="text" placeholder="Password"/>
+						<Form.Control size="sm" type="text" placeholder="Password" value={newPass} onChange={(e) => setNewPass(e.target.value)}/>
 					</Form.Group>
         </div>
       </Sb_Container>
@@ -43,13 +71,16 @@ export default function Settings () {
         <div style={{'width':'70%'}}>
           <Form.Group className="mb-3">
             <Form.Label htmlFor="Select"><Sb_Text font={16}>Language</Sb_Text></Form.Label>
-            <Form.Select size="sm" id="Select">
-              <option>English</option>
+            <Form.Select size="sm" id="Select" onChange={(e) => setLang(e.target.value)} value={lang}>
+              <option value={"ENGLISH"}>English</option>
+              <option value={"AMHARIC"}>አማርኛ</option>
             </Form.Select>
           </Form.Group>
         </div>
       </Sb_Container>
-      <Button size="sm" style={{'float':'right'}}><Sb_Text font={12} color="--lightGrey">Save Changes</Sb_Text></Button>
+      <Button size="sm" style={{'float':'right'}} onClick={() => saveChanges()}>
+        <Sb_Text font={12} color="--lightGrey">Save Changes</Sb_Text>
+      </Button>
     </Col>
   )
 }
