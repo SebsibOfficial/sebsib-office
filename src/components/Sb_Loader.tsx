1ed092ffba4203f52f9a446e8105002190213078
Yoseph <yosephten@gmail.com>
2022-04-13 16:45:52 +0300

    Added Sb_Loader

---
+++ src/components/Sb_Loader.tsx
@@ -0,0 +1,17 @@
+type Prop = {off?:boolean, full?:boolean}
+
+export default function Sb_Mini_Loader (prop:Prop) {
+  var {off = false, full = false} = prop;
+  return (
+    <>
+    {
+      !full ? <div style={{'display': off ? 'none':'flex', 'width':'30px', 'justifyContent':'center'}}>
+      <span style={{'left':'-30px'}} className='mini-loader'></span>
+    </div> : <div style={{'height': '100vh', 'display':'flex'}}>
+      <span className='loader'></span>
+    </div>
+    }
+    
+    </>
+  )
+}
