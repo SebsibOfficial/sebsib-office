c4cdde08df28c2c3bbd25842b621ccf2847ea4a7
y_ohanne_s <yt61312@gmail.com>
2022-07-19 22:06:54 +0300

    complete translation functionality

--- src/screens/Landing/Nav/Nav.tsx
+++ src/screens/Landing/Nav/Nav.tsx
@@ -1,71 +1,148 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../states/AuthContext";
 import { decodeJWT } from "../../../utils/helpers";
-import Logo from '../../../assets/logo.png';
+import Logo from "../../../assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArchive, faCog, faGripVertical, faHamburger, faThLarge, faTimes, faUsers } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArchive,
+  faCog,
+  faGripVertical,
+  faHamburger,
+  faThLarge,
+  faTimes,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 export default function Nav() {
-	const { token, setAuthToken } = useAuth();
-	function toWhere() {
-		if (token == "")
-			return "/login";
-		else if (decodeJWT(token as string).exp < new Date().getTime() / 1000) {
-			setAuthToken(""); return "/login";
-		}
-		else {
-			return "/dashboard";
-		}
-	}
-	function scrollTo(to: string) {
-		const section = document.querySelector(to);
-		section?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-	};
-	function menubar (cmd: boolean) {
-		cmd ? document.getElementsByClassName('menu-bar')[0].classList.add('show-menu-bar') : document.getElementsByClassName('menu-bar')[0].classList.remove('show-menu-bar');
-	}
-	return (
-		<nav>
-			<div className="logo_land">
-				<Link to={'/'} onClick={() => scrollTo('#start')}><img src={Logo} alt="" /></Link>
-			</div>
-			<div className="options">
-				<div className="list_cont">
-					<Link to={'/'}><div className="list_item">ስለ እኛ</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={'/pricing'}><div className="list_item">ዋጋችን</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={'/'} state={true} onClick={() => scrollTo('#contact-us')}><div className="list_item">አግኙን</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={toWhere()} state={true}><div className="list_item">ይግቡ</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={'/register'}><div className="list_item" id='reg_but'>ይመዝገቡ</div></Link>
-				</div>
-				<div className="lang">
-					<span id='amh'>አም</span>
-					<span id='eng'>EN</span>
-				</div>
-			</div>
-			<div className="menu">
-				<FontAwesomeIcon icon={faGripVertical} onClick={() => menubar(true)}/>
-			</div>
-			<div className="menu-bar">
-					<div className="list_item netela" style={{'marginBottom':'1em'}}>
-						<FontAwesomeIcon icon={faTimes} onClick={() => menubar(false)}/>
-					</div>
-					<Link to={'/'} onClick={() => menubar(false)}><div className="list_item">ስለ እኛ</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={'/pricing'} onClick={() => menubar(false)}><div className="list_item">ዋጋችን</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={'/'} state={true} onClick={() => {menubar(false)}}><div className="list_item">አግኙን</div></Link>
-					<div className="list_item netela">፣</div>
-					<Link to={toWhere()} onClick={() => menubar(false)} state={true}><div className="list_item">ይግቡ</div></Link>
-					<div className="lang">
-						<span id='amh'>አም</span>
-						<span id='eng'>EN</span>
-					</div>
-					<Link to={'/register'} onClick={() => menubar(false)}><div className="list_item" id='reg_but'>ይመዝገቡ</div></Link>
-			</div>
-		</nav>
-	)
+  const { token, setAuthToken } = useAuth();
+  function toWhere() {
+    if (token == "") return "/login";
+    else if (decodeJWT(token as string).exp < new Date().getTime() / 1000) {
+      setAuthToken("");
+      return "/login";
+    } else {
+      return "/dashboard";
+    }
+  }
+  function scrollTo(to: string) {
+    const section = document.querySelector(to);
+    section?.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+  function menubar(cmd: boolean) {
+    cmd
+      ? document
+          .getElementsByClassName("menu-bar")[0]
+          .classList.add("show-menu-bar")
+      : document
+          .getElementsByClassName("menu-bar")[0]
+          .classList.remove("show-menu-bar");
+  }
+
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <nav>
+      <div className="logo_land">
+        <Link to={"/"} onClick={() => scrollTo("#start")}>
+          <img src={Logo} alt="" />
+        </Link>
+      </div>
+      <div className="options">
+        <div className="list_cont">
+          <Link to={"/"}>
+            <div className="list_item">{t("nav_items.about_us")}</div>
+          </Link>
+          <div className="list_item netela">፣</div>
+          <Link to={"/pricing"}>
+            <div className="list_item">{t("nav_items.pricing")}</div>
+          </Link>
+          <div className="list_item netela">፣</div>
+          <Link to={"/"} state={true} onClick={() => scrollTo("#contact-us")}>
+            <div className="list_item">{t("nav_items.contact_us")}</div>
+          </Link>
+          <div className="list_item netela">፣</div>
+          <Link to={toWhere()} state={true}>
+            <div className="list_item">{t("nav_items.login")}</div>
+          </Link>
+          <div className="list_item netela">፣</div>
+          <Link to={"/register"}>
+            <div className="list_item" id="reg_but">
+              {t("nav_items.register")}
+            </div>
+          </Link>
+        </div>
+        <div className="lang">
+          <span
+            id="amh"
+            onClick={() => changeLanguage("am")}
+            style={{ opacity: i18n.language == "am" ? "1" : "0.3" }}
+          >
+            አም
+          </span>
+          <span
+            id="eng"
+            onClick={() => changeLanguage("en")}
+            style={{ opacity: i18n.language == "en" ? "1" : "0.3" }}
+          >
+            EN
+          </span>
+        </div>
+      </div>
+      <div className="menu">
+        <FontAwesomeIcon icon={faGripVertical} onClick={() => menubar(true)} />
+      </div>
+      <div className="menu-bar">
+        <div className="list_item netela" style={{ marginBottom: "1em" }}>
+          <FontAwesomeIcon icon={faTimes} onClick={() => menubar(false)} />
+        </div>
+        <Link to={"/"} onClick={() => menubar(false)}>
+          <div className="list_item">{t("nav_items.about_us")}</div>
+        </Link>
+        <div className="list_item netela">፣</div>
+        <Link to={"/pricing"} onClick={() => menubar(false)}>
+          <div className="list_item">{t("nav_items.pricing")}</div>
+        </Link>
+        <div className="list_item netela">፣</div>
+        <Link
+          to={"/"}
+          state={true}
+          onClick={() => {
+            menubar(false);
+          }}
+        >
+          <div className="list_item">{t("nav_items.contact_us")}</div>
+        </Link>
+        <div className="list_item netela">፣</div>
+        <Link to={toWhere()} onClick={() => menubar(false)} state={true}>
+          <div className="list_item">{t("nav_items.login")}</div>
+        </Link>
+        <div className="lang">
+          <span
+            id="amh"
+            onClick={() => changeLanguage("am")}
+            style={{ opacity: i18n.language == "am" ? "1" : "0.3" }}
+          >
+            አም
+          </span>
+          <span
+            id="eng"
+            onClick={() => changeLanguage("en")}
+            style={{ opacity: i18n.language == "en" ? "1" : "0.3" }}
+          >
+            EN
+          </span>
+        </div>
+        <Link to={"/register"} onClick={() => menubar(false)}>
+          <div className="list_item" id="reg_but">
+            {t("nav_items.register")}
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 }
--- src/screens/Landing/landing_content.ts
+++ src/screens/Landing/landing_content.ts
@@ -1,42 +1,43 @@
-import I1 from '../../assets/Ill-coins.svg';
-import I2 from '../../assets/Ill-contact.svg';
-import I3 from '../../assets/Ill-gursh.svg';
-import I4 from '../../assets/Ill-ques.svg';
-import I5 from '../../assets/Ill-sec.svg';
-import I6 from '../../assets/Ill-time.svg';
-import I7 from '../../assets/Ill-hager.svg';
+import I1 from "../../assets/Ill-coins.svg";
+import I2 from "../../assets/Ill-contact.svg";
+import I3 from "../../assets/Ill-gursh.svg";
+import I4 from "../../assets/Ill-ques.svg";
+import I5 from "../../assets/Ill-sec.svg";
+import I6 from "../../assets/Ill-time.svg";
+import I7 from "../../assets/Ill-hager.svg";
+import { useTranslation } from "react-i18next";
 
 const data = [
-    {
-        headline: "ሰብስብ ምንድን ነው",
-        subtext: "ሰብስብ በ ኢትዮጵያ የመጀመሪያ የሆነ የመረጃ መሰብሰቢያ መተግበርያ ነው። በእጅ ስልክዎ እና በ ኮምፒተሮ በመታገዝ የተለያዩ የ ዳሰሳ ጥናቶችን ለመስራት አመቺ የሆነ ቁስ ነው ።",
-        illustration: I4
-    },
-    {
-        headline: "በጣም ቀላል",
-        subtext: "በአጭር ስልጠና እንዲሁም ግልጽ እና ባልተወሳሰበ የ እይታ መረብ ፤ በ ቀላሉ መረጃ የመስብስበ ስራዎትን ያቀላጥፉ።",
-        illustration: I3
-    },
-    {
-        headline: "አስተማማኝ ደህንነት",
-        subtext: "በሰብስብ አማክክኝነት የሚሰበሰብ ማንኛውም መረጃ አልያም የሚካሄድ የ ዳሰሳ ጥናት ፍጹም ደህንነቱ የተጠበቀ እና አስተማማኝ ነው።",
-        illustration: I5
-    },
-    {
-        headline: "በማይታመን ዋጋ",
-        subtext: "ከተፎካካሪዎቻችን አንጻር እጅግ አንስተኛ ዋጋ ፤ የውጭ ምንዛሪ ይሚያስቀር።",
-        illustration: I1
-    },
-    {
-        headline: "ሀገር ያፈራው",
-        subtext: "በኢትዮጵያውያን ፣ ለኢትዮጵያውያን ፣ ከኢትዮጵያውያን። ሃገራችንን ያማከለ ፣ ያለ ኢንተርኔት የሚሰራ ፣ በተለያዩ የኢትዮጵያ ቇንቇዎች የተዘጋጀ።",
-        illustration: I7
-    },
-    {
-        headline: "ሁሌም አለን",
-        subtext: "ሰብስብን ለመጠቀም የሚረዱ ስልጠናዎችን ፣ የ ብልሽት ጥገና እንዲሁም ለሚያግጥሞት ማንኛውም ጥያቄ በ ማንኛውም ሰዕት እንገኛለን።",
-        illustration: I6
-    }    
-]
+  {
+    headline: "second_page.headline",
+    subtext: "second_page.subtext",
+    illustration: I4,
+  },
+  {
+    headline: "third_page.headline",
+    subtext: "third_page.subtext",
+    illustration: I3,
+  },
+  {
+    headline: "fourth_page.headline",
+    subtext: "fourth_page.subtext",
+    illustration: I5,
+  },
+  {
+    headline: "fifth_page.headline",
+    subtext: "fifth_page.subtext",
+    illustration: I1,
+  },
+  {
+    headline: "sixth_page.headline",
+    subtext: "sixth_page.subtext",
+    illustration: I7,
+  },
+  {
+    headline: "seventh_page.headline",
+    subtext: "seventh_page.subtext",
+    illustration: I6,
+  },
+];
 
 export default data;
