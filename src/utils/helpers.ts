946997930ea9e8a5d35c9cc5cd949694fd79758e
Yoseph <yosephten@gmail.com>
2022-10-07 23:16:33 +0300

    Refactoring TranslateID

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,6 +4,7 @@ import { createObjectID } from 'mongo-object-reader';
 /* eslint-enable */
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
+import TYPES from './id-text.json';
 
 interface Token {
   _id: string,
@@ -52,50 +53,18 @@ export function decodeJWT(token:string) {
 
 export function translateIds (from: "ID" | "TEXT", inp: string) {
   if (from == 'ID') {
-    switch (inp) {
-      case "624558d1a263f17689cdc5bc":
-        return "TEXT";
-      case "624558d1a263f17689cdc5bd":
-        return "CHOICE";
-      case "624558d1a263f17689cdc5be":
-        return "MULTI-SELECT";
-      case "623cc24a8b7ab06011bd1e5f":
-        return "MEMBER";
-      case "623cc24a8b7ab06011bd1e60":
-        return "OWNER";
-      case "623cc24a8b7ab06011bd1e61":
-        return "ADMIN";
-      case "623cc24a8b7ab06011bd1e62":
-        return "SUPER";
-      case "623d73a051e8bcb894b3f7df":
-        return "FREE TRAIL";
-      case "623d73a051e8bcb894b3f7e0":
-        return "UNLIMITED";      
-      default:
-        return false;
+    for (let index = 0; index < TYPES.length; index++) {
+      const element = TYPES[index];
+      if (element._id == inp){
+        return element.text;
+      }
     }
   } else {
-    switch (inp) {
-      case "TEXT":
-        return "624558d1a263f17689cdc5bc";
-      case "CHOICE":
-        return "624558d1a263f17689cdc5bd";
-      case "MULTI-SELECT":
-        return "624558d1a263f17689cdc5be";
-      case "MEMBER":
-        return "623cc24a8b7ab06011bd1e5f";
-      case "OWNER":
-        return "623cc24a8b7ab06011bd1e60";
-      case "ADMIN":
-        return "623cc24a8b7ab06011bd1e61";
-      case "SUPER":
-        return "623cc24a8b7ab06011bd1e62";
-      case "FREE TRAIL":
-        return "623d73a051e8bcb894b3f7df";
-      case "UNLIMITED":
-        return "623d73a051e8bcb894b3f7e0";      
-      default:
-        return false;
+    for (let index = 0; index < TYPES.length; index++) {
+      const element = TYPES[index];
+      if (element.text == inp){
+        return element._id;
+      }
     }
   }
 }
