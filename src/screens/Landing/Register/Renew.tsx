204d0295c11e336e40248f43fbece2696c8a207f
Yoseph <yosephten@gmail.com>
2022-12-08 22:33:01 +0300

    Account number hide

--- src/screens/Landing/Register/Renew.tsx
+++ src/screens/Landing/Register/Renew.tsx
@@ -273,9 +273,9 @@ export default function Register() {
       width={30}>
           <>        
               <p><Sb_Text>Pay with your preferred bank to the below accounts and input the Transaction Reference Number</Sb_Text></p>
-              <p><Sb_Text>CBE: </Sb_Text><Sb_Text weight={900}>1000546715467890</Sb_Text> <Sb_Text> Or Dashen:</Sb_Text><Sb_Text weight={900}>55467854678910</Sb_Text></p>
+              <p><Sb_Text>CBE: </Sb_Text><Sb_Text weight={900}>100*********7890</Sb_Text> <Sb_Text> Or Dashen:</Sb_Text><Sb_Text weight={900}>554*********10</Sb_Text></p>
               <p></p>
-              <p><Sb_Text>If you are facing any issues contact us at support@sebsib.com or +251912345678</Sb_Text></p>
+              <p><Sb_Text>If you are facing any issues contact us at support@sebsib.com or +251920642556</Sb_Text></p>
           </>
         </Sb_Modal>
     </div>
