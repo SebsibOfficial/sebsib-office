91865340c1e666da3abdd26aa4f1c20ff7add5a8
Yoseph <yosephten@gmail.com>
2023-04-08 17:16:10 +0300

    Style change to loader

--- src/components/Sb_Loader.tsx
+++ src/components/Sb_Loader.tsx
@@ -1,13 +1,13 @@
-type Prop = {off?:boolean, full?:boolean}
+type Prop = {off?:boolean, full?:boolean, colored?:boolean}
 
 export default function Sb_Loader (prop:Prop) {
-  var {off = false, full = false} = prop;
+  var {off = false, full = false, colored = false} = prop;
   return (
     <>
     {
       !full ? 
       <div style={{'display': off ? 'none':'flex', 'width':'30px', 'justifyContent':'center'}}>
-        <div className="mini-spinner rounded-circle">
+        <div className={`mini-spinner rounded-circle ${colored ? `colored-spinner` : ``}`}>
         </div>
       </div> : 
       <div style={{'height': '70vh', 'display':'flex'}}>
